a2b82cf579a878288a55624255673b4e4370adb0
Kelley Chaplain <kmchaplain@gmail.com>
2019-05-06 07:06:57 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -3,9 +3,9 @@ require 'pry'
 class Hash
   def keys_of(arguments)
     arr = [ ]
-    arguments.split(", ").map do |item| 
+    arguments.to_s.split(", ").map do |item| 
       self.map do |k, v|
-        if v == item
+        if v == item || v == item.to_i
           arr << k
         end
       end
